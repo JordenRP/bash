dec929ddcc78f93a6ec265d5a61e4a2efc1110d4
pavel <gritsatsuev.p@promail366.com>
2023-11-14 23:03:53 +0400

    add script for ssh key

---
+++ setup_ssh.sh
@@ -0,0 +1,39 @@
+ 
+#!/bin/bash
+
+# Проверяем количество переданных аргументов
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <Host> <Hostname>"
+    exit 1
+fi
+
+HOST=$1
+HOSTNAME=$2
+SSH_DIR="$HOME/.ssh"
+CONFIG="$SSH_DIR/config"
+KEY="$SSH_DIR/$HOST"
+
+# Создаем директорию .ssh, если она не существует
+mkdir -p "$SSH_DIR"
+
+# Проверяем существует ли файл config и создаем его, если нет
+if [ ! -f "$CONFIG" ]; then
+    echo "Host *" > "$CONFIG"
+    echo "    AddKeysToAgent yes" >> "$CONFIG"
+    echo "    UseKeychain yes" >> "$CONFIG"
+    echo "    IdentityFile ~/.ssh/id_rsa" >> "$CONFIG"
+fi
+
+# Генерируем SSH ключ, если он не существует
+if [ ! -f "$KEY" ]; then
+    ssh-keygen -t rsa -C "gritsatsuev.pavel@gmail.com" -f "$KEY" -N ""
+    echo "SSH key generated at $KEY"
+fi
+
+# Добавляем новую конфигурацию в файл config
+echo -e "Host $HOST\n    Hostname $HOSTNAME\n    IdentityFile=$KEY" >> "$CONFIG"
+echo "Config entry added for $HOST"
+
+# Отображение результата конфигурации
+echo "Current SSH configuration for $HOST:"
+grep -A 2 "Host $HOST" "$CONFIG"
