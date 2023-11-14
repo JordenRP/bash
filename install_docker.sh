ec65bc7b63cda6dd3f7258d3727574ef5d28741c
pavel <gritsatsuev.p@promail366.com>
2023-11-14 22:34:33 +0400

    add docker install scripts

---
+++ install_docker.sh
@@ -0,0 +1,24 @@
+ #!/bin/bash
+
+# Обновление списка пакетов
+sudo apt update
+
+# Установка необходимых зависимостей
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+# Добавление GPG ключа для репозитория Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Добавление репозитория Docker в APT
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Обновление списка пакетов после добавления репозитория Docker
+sudo apt update
+
+# Установка Docker
+sudo apt install docker-ce -y
+
+# Проверка запуска Docker
+sudo systemctl status docker
+
+sudo usermod -aG docker username
